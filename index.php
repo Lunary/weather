b0afc2dd3ae252cfb64f983bda429c6e22fbdf56
hackLover <hacklover99@gmail.com>
2013-04-11 02:27:12 +0300

    Correctly indent

--- index.php
+++ index.php
@@ -5,17 +5,18 @@
 	<title>Weather demo - made by hackLover</title>
 	<link href="themes/style.css" media="all" rel="stylesheet" type="text/css" />
 </head>
-<body>
-<div class="top"></div>
-<?php
-
-include "hl.weather.php";
-$names_month = array("Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre");
-echo show_weather("YOUR_API_KEY", "IT", "Brescia", 3, "c", $names_month);
-?>
-<div class="copy">
-	made by <a href="http://www.hacklover.net">hackLover</a><br />
-	donate to <span class="btc">1GTiuK78RNS6LHPekZ3bo7cCUaaVHG75GE</span>
-</div>
-</body>
+	<body>
+		<div class="top"></div>
+		
+		<?php
+			include "hl.weather.php";
+			$names_month = array("Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre");
+			echo show_weather("YOUR_API_KEY", "IT", "Brescia", 3, "c", $names_month);
+		?>
+		
+		<div class="copy">
+			made by <a href="http://www.hacklover.net">hackLover</a><br />
+			donate to <span class="btc">1GTiuK78RNS6LHPekZ3bo7cCUaaVHG75GE</span>
+		</div>
+	</body>
 </html>
