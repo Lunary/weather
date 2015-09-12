5acd7675b3146f159cfb47bb87de57472370a29a
Lunary <all.farafonov@gmail.com>
2015-09-12 15:07:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<meta charset="UTF-8" />
-		<title>Weather demo - made by hackLover</title>
+		<title>Gleisdorf Wetter</title>
 		<link href="themes/style.css" media="all" rel="stylesheet" type="text/css" />
 	</head>
 	<body>
@@ -10,13 +10,8 @@
 		
 		<?php
 			include "hl.weather.php";
-			$names_month = array("Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre");
-			echo show_weather("YOUR_API_KEY", "IT", "Brescia", 3, "c", $names_month);
+			$names_month = array("Januar", "Februar", "März", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Dezember");
+			echo show_weather("YOUR_API_KEY", "AT", "Gleisdorf", 3, "c", $names_month);
 		?>
-		
-		<div class="copy">
-			made by <a href="http://www.hacklover.net">hackLover</a><br />
-			donate to <span class="btc">1GTiuK78RNS6LHPekZ3bo7cCUaaVHG75GE</span>
-		</div>
 	</body>
 </html>
